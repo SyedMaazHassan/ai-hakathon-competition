12264bfd0a20498044106172c836cfa12166ca42
Hamza Bilal <hamzaabialal@gmail.com>
2025-09-27 18:51:00 +0500

    use postgres instead of dbSqlite3

--- config/settings/base.py
+++ config/settings/base.py
@@ -100,8 +100,12 @@ WSGI_APPLICATION = 'config.wsgi.application'
 # Database (default SQLite; overridden in production)
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': os.environ.get('DB_NAME', 'django_db'),
+        'USER': os.environ.get('DB_USER', 'django'),
+        'PASSWORD': os.environ.get('DB_PASSWORD', 'django'),
+        'HOST': os.environ.get('DB_HOST', 'db'),  # matches your docker-compose service name
+        'PORT': os.environ.get('DB_PORT', 5432),
     }
 }
 
